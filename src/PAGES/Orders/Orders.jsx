2643835b4af7d7a81d2c6e3350bca12699f441dd
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-12 22:01:03 +0600

    add delete method

--- src/PAGES/Orders/Orders.jsx
+++ src/PAGES/Orders/Orders.jsx
@@ -5,20 +5,24 @@ import OrdersTable from "./OrdersTable";
 const Orders = () => {
   const { user } = useContext(AuthContext);
   const [orders, setOrders] = useState([]);
+  const [deleteLodaer, setDeleteLoader] = useState(false);
 
   useEffect(() => {
     fetch(`http://localhost:5000/order?email=${user?.email}`)
       .then((res) => res.json())
       .then((data) => setOrders(data));
-  }, []);
+  }, [deleteLodaer]);
   console.log(orders);
   return (
     <>
+    {
+        orders.length
+    }
       <div className="overflow-x-auto w-full">
         <table className="table w-full">
           <tbody>
             {orders.map((order) => (
-              <OrdersTable key={order._id} orders={order}></OrdersTable>
+              <OrdersTable key={order._id} orders={order} setDeleteLoader={setDeleteLoader} deleteLodaer={deleteLodaer}></OrdersTable>
             ))}
           </tbody>
         </table>
--- src/PAGES/Orders/OrdersTable.jsx
+++ src/PAGES/Orders/OrdersTable.jsx
@@ -1,13 +1,49 @@
 import { RiChatDeleteFill } from "react-icons/ri";
+import Swal from "sweetalert2";
 
-const OrdersTable = ({ orders }) => {
-  const { img, title, email, price, date } = orders;
+const OrdersTable = ({ orders, setDeleteLoader, deleteLodaer }) => {
+  const {_id, img, title, email, price, date } = orders;
   console.log(orders);
+
+  const handleDelete = (id) =>{
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Deleted!',
+            'Your order has been deleted.',
+            'success'
+          )
+
+        fetch(`http://localhost:5000/order/${id}`, {
+            method: "DELETE"
+        })
+        .then(res => res.json())
+        .then(data => setDeleteLoader(!deleteLodaer))
+
+        // console.log(id);
+        }
+      })
+      
+
+  }
+
+
+
+
+
   return (
     <>
       <tr>
         <th>
-          <button>
+          <button onClick={() => handleDelete(_id)}>
             <RiChatDeleteFill className="text-4xl"></RiChatDeleteFill>
           </button>
         </th>
